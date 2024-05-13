c0207f054e6b1d6b92242617536b4f9a13a97276
mohammadnoor <mohammadnoorcse@gmail.com>
2024-05-13 18:46:22 +0600

    working fetch data

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -6,4 +6,42 @@ import jwt from "jsonwebtoken";
 
 export async function POST(req){
    await connectDB();
+   const {email,password} = await req.json();
+   if(!email || !password){
+      return NextResponse.json({
+         msg: "All Fields are  Mandatory !"
+      })
+   }
+
+   const emailExists = await User.findOne({email});
+   if(!emailExists){
+      return NextResponse.json({
+         msg:"Please Register first !"
+      })
+   }
+
+   const passwordMatched = await bcrypt.compare(password,emailExists.password);
+   try {
+      if(passwordMatched){
+         const token = jwt.sign({token:emailExists._id},process.env.JWT_SECRET,{
+            expiresIn: "30d",
+         })
+         return NextResponse.json({
+            msg:"Logged in successfully !",
+            token
+         })
+      }else{
+         return NextResponse.json({
+            msg:"Please Correct Email and Password !"
+         })
+      }
+
+   } catch (error) {
+      return NextResponse.json({
+         msg:"Error loggin user",
+         error:error.message
+      })
+   }
+
+
 }
--- app/components/Header1.jsx
+++ app/components/Header1.jsx
@@ -1,12 +1,37 @@
+"use client";
 import logo from "@/public/image/logo.png"
 import Image from "next/image"
 import Block from "./Block"
 import demo from "@/public/image/demo.svg";
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/navigation';
+import Link from "next/link";
+
 
 function Header1() {
+  const [auth,setAuth] = useState(false);
+  useEffect(()=>{
+    const key = localStorage.getItem("user");
+    if(key){
+      setAuth(true);
+      return;
+    }
+    setAuth(false);
+  },[auth]);
+
+  const router = useRouter();
+ 
+
+  const handleLogout=()=>{
+    localStorage.removeItem("user");
+    setAuth(false);
+    router.push("/");
+    
+    
+  }
   return (
     <div className=" flex justify-between border-b-2 border-gray-300 items-center h-24 px-10">
-         <Image
+      <Image
         src={logo}
         alt="logo"
         width={200}
@@ -14,27 +39,35 @@ function Header1() {
         className=" w-28 h-28 "
       />
       <div className=" h-full flex">
-
-      <Block title={"Become a member"} para={"Additional 0% off on stays."} />
+        <Block title={"Become a member"} para={"Additional 0% off on stays."} />
         <Block
           title={"OYO for business"}
           para={"Trusted by 5000 corporates."}
         />
         <Block title={"List your property"} para={"Start earning in 30 min."} />
         <Block title={"987654321"} para={"Call us to book now."} />
-
         <div className="flex items-center px-3 ">
-        <Image
+          <Image
             src={demo}
             alt="demo"
-            width={200}
-            height={200}
+            // width={200}
+            // height={200}
             className=" w-10 h-10 rounded-full mr-5"
           />
-          <h3 className="font-bold">Login /Signup</h3>
+          {auth ? (
+            <h3
+              className=" font-bold cursor-pointer"
+              onClick={handleLogout}
+            >
+              Logout
+            </h3>
+          ) : (
+            <Link href={"/login"} className=" font-bold">
+              Login / Signup
+            </Link>
+          )}
         </div>
       </div>
-     
     </div>
   )
 }
